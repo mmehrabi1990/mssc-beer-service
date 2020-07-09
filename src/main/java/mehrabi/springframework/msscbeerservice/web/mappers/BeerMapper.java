2ee643016acd20f58192ec9e471c135db4bf0f5e
Mohammad Mehrabi <mmehrabi1990@yahoo.com>
2020-07-09 12:57:47 +0300

    updating UPC. closes #17

--- src/main/java/mehrabi/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/mehrabi/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -3,10 +3,14 @@ package mehrabi.springframework.msscbeerservice.web.mappers;
 import mehrabi.springframework.msscbeerservice.domain.Beer;
 import mehrabi.springframework.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
+    @Mapping(target = "quantityOnHand", source = "")
     BeerDto beerToBeerDTO(Beer beer);
 
+    @Mapping(target = "quantityToBrew", source = "")
+    @Mapping(target = "minOnHand", source = "")
     Beer beerDTOToBeer(BeerDto beerDTO);
 }
